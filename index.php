7a89ae07d29aa483b98066ad0faef8e41b4f5f89
agamenon <agamenon_junior1@hotmail.com>
2024-08-11 23:12:56 -0300

    Commit Inicial

--- index.php
+++ index.php
@@ -4,39 +4,46 @@ session_start();
 ?>
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  </head>
-  <body>
+</head>
 
-        <div class="navbar navbar-nav  bg-dark bg-gradient">
-            <div class="container-fluid">
-                <div class="navbar-header ">
-                    <a href="#" class="navbar-brand text-white">Classificadão</a>
-                </div>
+<body>
 
-                <ul class="nav navbar navbar-right">
-                    <li class="nav-item"><a href="#Cadastrar" class="nav-link text-white">Cadastrar-se</a></li>
-                    <li class="nav-item"><a href="#Entrar" class="nav-link text-white">Entrar</a></li>
-                </ul>
-            </div>
-        </div>
+    <div class="navbar navbar-nav  bg-dark bg-gradient">
         <div class="container-fluid">
-            <div class="container bg-secondary bg-gradient p-2 mt-2 text-center text-white shadow p-3 mb-5 rounded">
-                <h2>Batemos a meta de 30 milhões de anúncios ativos !</h2>
-                <h2>Contamos com 1 milhão de usuários ativos</h2>
+            <div class="navbar-header ">
+                <a href="#" class="navbar-brand text-white">Classificadão</a>
             </div>
+
+            <ul class="nav navbar navbar-right">
+                <li class="nav-item"><a href="#Cadastrar" class="nav-link text-white">Cadastrar-se</a></li>
+                <li class="nav-item"><a href="#Entrar" class="nav-link text-white">Entrar</a></li>
+            </ul>
         </div>
+    </div>
+    <div class="container-fluid">
+        <div class="container bg-secondary bg-gradient p-2 mt-2 text-center text-white shadow p-3 mb-5 rounded">
+            <h2>Batemos a meta de 30 milhões de anúncios ativos !</h2>
+            <h2>Contamos com 1 milhão de usuários ativos</h2>
+        </div>
+    </div>
+    <div class="container">
         <div class="row">
             <div class="col-sm-3">Filtro</div>
             <div class="col-sm-9">Últimos Anúncios</div>
         </div>
 
-  
-  
+    </div>
+
+
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  </body>
+</body>
+
 </html>
