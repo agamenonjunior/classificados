bf1247661a1be8c767574460d578723f4a6e64af
agamenon <agamenon_junior1@hotmail.com>
2024-08-11 23:11:12 -0300

    Commit Inicial

--- index.php
+++ index.php
@@ -30,6 +30,10 @@ session_start();
                 <h2>Contamos com 1 milhão de usuários ativos</h2>
             </div>
         </div>
+        <div class="row">
+            <div class="col-sm-3">Filtro</div>
+            <div class="col-sm-9">Últimos Anúncios</div>
+        </div>
 
   
   
