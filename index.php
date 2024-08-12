7cb532d4b00919d8420f6b95fda9eb8ada562716
agamenon <agamenon_junior1@hotmail.com>
2024-08-11 23:09:29 -0300

    Commit Inicial

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -26,7 +30,7 @@
                 <h2>Contamos com 1 milhão de usuários ativos</h2>
             </div>
         </div>
-        
+
   
   
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
