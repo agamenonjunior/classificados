925c4ce57270afa5cabcbc17f70de23098b84d3c
agamenon <agamenon_junior1@hotmail.com>
2024-08-24 11:59:53 -0300

    Refatorando Classes

---
+++ Classes/Contato.php
@@ -0,0 +1,72 @@
+<?php
+
+class Contato{
+
+    private $id;
+    private $id_user;
+    private $numero;
+    private $endereco;
+
+    public function __construct($id_user,$numero,$endereco) {
+        $this->id_user  = $id_user;
+        $this->numero   = $numero;
+        $this->endereco = $endereco;
+    }
+
+
+    /**
+     * Get the value of id_user
+     */
+    public function getIdUser()
+    {
+        return $this->id_user;
+    }
+
+    /**
+     * Set the value of id_user
+     */
+    public function setIdUser($id_user): self
+    {
+        $this->id_user = $id_user;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of numero
+     */
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    /**
+     * Set the value of numero
+     */
+    public function setNumero($numero): self
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of endereco
+     */
+    public function getEndereco()
+    {
+        return $this->endereco;
+    }
+
+    /**
+     * Set the value of endereco
+     */
+    public function setEndereco($endereco): self
+    {
+        $this->endereco = $endereco;
+
+        return $this;
+    }
+}
+
+?>
--- Classes/User.php
+++ Classes/User.php
@@ -2,14 +2,13 @@
 
 class User {
     
+    private $id;
     private $nome;
     private $email;
     private $senha;
     private $contato;
     
-    public function Cadastrar($nome,$email,$senha,$contato){
-
-        
+    public function Cadastrar($nome,$email,$senha,$contato){        
 
     }
     public function Editar($id){
@@ -25,4 +24,94 @@ class User {
     }
 
     
+
+    /**
+     * Get the value of id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome($nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     */
+    public function setEmail($email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of senha
+     */
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    /**
+     * Set the value of senha
+     */
+    public function setSenha($senha): self
+    {
+        $this->senha = $senha;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of contato
+     */
+    public function getContato()
+    {
+        return $this->contato;
+    }
+
+    /**
+     * Set the value of contato
+     */
+    public function setContato($contato): self
+    {
+        $this->contato = $contato;
+
+        return $this;
+    }
 }
